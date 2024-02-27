8834d270606f9f64c32295315b3018c396205f4d
JoaoSouza12 <joao.sousa.12145@gmail.com>
2024-02-27 19:22:54 -0300

    Refatoração o TokenType

--- lab-jackcompiler-master/src/main/java/br/ufma/ecp/token/TokenType.java
+++ lab-jackcompiler-master/src/main/java/br/ufma/ecp/token/TokenType.java
@@ -1,46 +1,83 @@
 package br.ufma.ecp.token;
 
-import java.util.List;
-import java.util.Map;
+import java.util.Arrays;
+
 public enum TokenType {
-    PLUS, MINUS,
+ 
+    
 
-    // Literals.
-    NUMBER,
-    STRING,
+    STRING(),
 
-    IDENT,
-    EOF,
+    INTEGER(),
 
-    // symbols
-    LPAREN, RPAREN,
-    LBRACE, RBRACE,
-    LBRACKET, RBRACKET,
+    IDENTIFIER(),
 
-    COMMA, SEMICOLON, DOT, ASTERISK, SLASH,
-    AND, OR, NOT,
-    LT, GT, EQ,
     // keywords
-    WHILE, CLASS, CONSTRUCTOR, FUNCTION,
-    METHOD, FIELD, STATIC, VAR, INT,
-    CHAR, BOOLEAN, VOID, TRUE, FALSE,
-    NULL, THIS, LET, DO, IF, ELSE, RETURN,
-    ILLEGAL;
+    WHILE("while"), CLASS("class"),CONSTRUCTOR("constructor"),FUNCTION("function"),
+    METHOD("method"),FIELD("field"),STATIC("static"),VAR("var"),INT("int"),
+    CHAR("char"),BOOLEAN("boolean"),VOID("void"),TRUE("true"),FALSE("false"),
+    NULL("null"),THIS("this"),LET("let"),DO("do"),IF("if"),
+    ELSE("else"), RETURN("return"),
 
-    static public boolean isSymbol (String c) {
-        String symbols = "{}()[].,;+-*/&|<>=~";
-        return symbols.indexOf(c) > -1;
 
+    // Symbols
+    PLUS("+"),
+    EQ("="),
+    MINUS("-"),
+    ASTERISK("*"),
+    SLASH("/"),
+    AND("&"),
+    OR("|"),
+    NOT("~"),
+    
+    LT("<"),
+    GT(">"),
+
+    DOT("."),
+    COMMA(","),
+    SEMICOLON(";"),
+    LPAREN("("),
+    RPAREN(")"),
+    LBRACE("{"),
+    RBRACE("}"),
+    LBRACKET("["),
+    RBRACKET("]"),
+
+
+    EOF();
+
+
+    private TokenType() {
     }
 
-    // static public boolean isKeyword(TokenType type) {
-    //     List<TokenType> keywords = List.of(
+    private TokenType(String value) {
+        this.value = value;
+    }
 
-    //             WHILE, CLASS, CONSTRUCTOR, FUNCTION,
-    //             METHOD, FIELD, STATIC, VAR, INT,
-    //             CHAR, BOOLEAN, VOID, TRUE, FALSE,
-    //             NULL, THIS, LET, DO, IF, ELSE, RETURN);
-    //     return keywords.contains(type);
-    // }
+    public String value;
 
+
+    public static TokenType fromValue(String value) {
+        return Arrays.stream(TokenType.values())
+                .filter(symbolType -> symbolType.value != null && symbolType.value.equals(value))
+                .findFirst()
+                .orElse(null);
+    }
+
+
+    static public boolean isSymbol (char c) {
+        String symbols = "{}()[].,;+-*/&|<>=~";
+        return symbols.indexOf(c) > -1;
+    }
+
+    static public boolean isOperator(TokenType type) {
+        return "+-*/<>=~&|".contains(type.value);
+    }
+
+    static public TokenType keyword (String value) {
+      return fromValue(value);
+    }
+
+
+    
 }
